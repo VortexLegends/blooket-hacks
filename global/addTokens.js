ecfff000c0209aeeb2a804dcbc881aea40b57620
BlazeGX_Gaming <160781429+VortexLegends@users.noreply.github.com>
2024-08-20 03:38:48 -0600

    Update addTokens.js
    more tokens and xp

--- global/addTokens.js
+++ global/addTokens.js
@@ -37,8 +37,8 @@
                         },
                         body: await encodeValues({
                             name: x.name,
-                            addedTokens: 500,
-                            addedXp: 300
+                            addedTokens: 50000,
+                            addedXp: 50000
                         }, e.secret)
                     });
                     fetch("https://api.blooket.com/api/users/add-rewards", {
